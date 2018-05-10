4f8781a471c5840512aa2d094bbe56f2be4e428c
lizhooh <960316408@qq.com>
2018-05-10 17:31:20 +0800

    add: index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+
+export interface IOptions {
+    devtool?: Boolean,
+    reducers: Object,
+    compose?: Array<any>,
+    applyMiddlewares?: Array<any>,
+}
+
+export default function (options: IOptions, applyMiddlewares: Array<any>): any;
+
